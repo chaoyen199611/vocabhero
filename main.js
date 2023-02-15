fcc154be8b9aac9896283869a1946f2955d0c421
Chao-Yen Huang <chaoyen510220@gmail.com>
2023-02-15 19:31:30 +0800

    add theme color

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
 const path = require('path')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
+    var win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, 'preload.js'),
         }
     })
   
     win.loadFile('index.html')
+    win.webContents.openDevTools()
+
+    ipcMain.handle('dark-mode:toggle', () => {
+        if (nativeTheme.shouldUseDarkColors) {
+            nativeTheme.themeSource = 'light'
+        } else {
+            nativeTheme.themeSource = 'dark'
+        }
+        return nativeTheme.shouldUseDarkColors
+    })
+
+    ipcMain.handle('dark-mode:system', () => {
+        nativeTheme.themeSource = 'system'
+    })
 }
 
 app.whenReady().then(() => {
@@ -24,3 +38,4 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
+
